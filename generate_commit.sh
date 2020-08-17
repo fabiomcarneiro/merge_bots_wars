a3c43b197188c34e62af20396d3f25dca71a4d78
Fabio Carneiro <fabio.carneiro@outlook.com>
2020-08-17 13:39:32 +0100

    another

--- generate_commit.sh
+++ generate_commit.sh
@@ -5,11 +5,11 @@ fn() {
 local x=1
 while [ $x -le 1 ]
 do
-  git checkout -b melon/$x && \
-  echo $x > melon$x && \
+  git checkout -b joe/$x && \
+  echo $x > joe$x && \
   git add -A . && \
   git commit -m another && \
-  git push origin melon/$x && \
+  git push origin joe/$x && \
   git checkout master
   (( x++ ))
 done
